5d852bae62a3086c93f2e6b31bed535b17ddef5d
renyuqi <316703799@qq.com>
2021-12-22 15:36:06 +0800

    修改位置

---
+++ cs-common/src/main/java/com/ryq/common/CommonApplication.java
@@ -0,0 +1,17 @@
+package com.ryq.common;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author Ren YuQi
+ * @Create 2021-12-22 11:12
+ */
+@SpringBootApplication
+public class CommonApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CommonApplication.class, args);
+    }
+
+}
